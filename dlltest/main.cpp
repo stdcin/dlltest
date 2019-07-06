03004f2c7cec029cbbaed01f3b96e496512d7dcf
vitaminc <3157029+stdcin@users.noreply.github.com>
2019-07-06 20:51:24 +0800

    update

---
+++ dlltest/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+	__declspec(dllexport) int add(int x, int y);
+	__declspec(dllexport) int sub(int x, int y);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+int add(int x, int y)
+{
+	printf("add(%d, %d)\n", x, y);
+	return x + y;
+}
+
+int sub(int x, int y)
+{
+	printf("sub(%d, %d)\n", x, y);
+	return x - y;
+}
